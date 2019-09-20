11676626254445dfaa0863fd660b1543f3071660
ajay.garipally@us.mcd.com <ajaygaripally.ag@gmail.com>
2019-09-20 22:45:31 +0200

    name change2

--- src/main/java/example/Hello.java
+++ src/main/java/example/Hello.java
@@ -1,5 +1,5 @@
 public class Hello{  
 	public static void main(String args[])throws Exception{  
-		  System.out.println("Hello Automated");
+		  System.out.println("Hello Automated test4");
 	}
 }  

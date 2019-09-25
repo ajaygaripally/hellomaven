70d727d3d078e8aa7c505bd1cac27caf2de33568
ajaygaripally <55584847+ajaygaripally@users.noreply.github.com>
2019-09-25 21:17:47 +0200

    Update Hello.java

--- src/main/java/example/Hello.java
+++ src/main/java/example/Hello.java
@@ -1,5 +1,5 @@
 public class Hello{  
 	public static void main(String args[])throws Exception{  
-		  System.out.println("Hello Automated Test9454fg656565");
+		  System.out.println("Hello Automated Test9454fg6565652323232");
 	}
 }  
